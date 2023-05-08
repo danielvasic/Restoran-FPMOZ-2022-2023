d5691c0e742810f37d517d339b838a4d28657861
dvasi_fpmoz <dvasic1@gmail.com>
2023-05-08 11:49:43 +0200

    Vježbe

--- src/main/java/ba/sum/fpmoz/restoran/controller/AuthController.java
+++ src/main/java/ba/sum/fpmoz/restoran/controller/AuthController.java
@@ -1,12 +1,20 @@
 package ba.sum.fpmoz.restoran.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ba.sum.fpmoz.restoran.model.User;
 
 @Controller
 public class AuthController {
     @GetMapping("/login")
     public String login () {
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String register (Model model) {
+        model.addAttribute("user", new User());
+        return "register";
     }
 }
